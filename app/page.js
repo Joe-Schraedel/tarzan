0ca54eae98fa3a57db9fdd828cc0db66e145f315
Joe-Schraedel <joeschraedel@gmail.com>
2025-01-15 16:18:06 -0700

    changed to blue

--- app/page.js
+++ app/page.js
@@ -13,8 +13,8 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 text-red-500">
-            Get started by doing itemsasdfaHEY JAYDENsdfadsf{" "}
+          <li className="mb-2 text-blue-500">
+            Get started by doing itemsasdfaHEY JAYDENgsdfadsf{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.jsadsfadsf
             </code>
